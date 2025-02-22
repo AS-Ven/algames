c2633cfede50b3d7c95c1c74b61ae9a99c6e5f61
fgrSto <aldric.vendas@gmail.com>
2025-02-22 01:15:35 +0100

    feat: peer

--- components/matching-marks/symbol-grid.tsx
+++ components/matching-marks/symbol-grid.tsx
@@ -8,6 +8,8 @@ const SymbolGrid = ({ difficulty = 3 }: SymbolGridProps) => {
 
     let suit: number[] = []
     let suitList: number[][] = []
+    let peerSuit: number[] = []
+    let peer: number[] = []
 
     for (let i = 0; i < difficulty * difficulty; i++) {
         suit = []
@@ -17,11 +19,23 @@ const SymbolGrid = ({ difficulty = 3 }: SymbolGridProps) => {
         suitList.push(suit)
     }
 
+    for (let i = 0; i < 36; i++) {
+        Math.floor(Math.random() * 2) ? peerSuit.push(1) : peerSuit.push(0)
+    }
+
+    for (let i = 0; i < 2; i++) {
+        peer.push(Math.floor(Math.random() * (difficulty * difficulty)))
+        suitList[peer[i]] = peerSuit
+    }
+
+    console.log(peer);
+    
+
     return (
-        <div className="flex flex-wrap gap-4 justify-center items-center h-screen">
+        <div className="flex flex-wrap flex-col gap-4 justify-center items-center h-screen">
             {
                 Array.from({ length: difficulty }, (_, i) => (
-                    <div key={i} className="flex flex-col gap-4">
+                    <div key={i} className="flex gap-4">
                         {
                             Array.from({ length: difficulty }, (_, j) => (
                                 <Symbol
