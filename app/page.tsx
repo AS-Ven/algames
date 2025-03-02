bb201414380e7fbbc8c91b731ff23f30ee80a75a
fgrSto <aldric.vendas@gmail.com>
2025-03-02 19:00:11 +0100

    fix: merge

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Menu from "@/components/global/menu";
-
 const Home = async () => {
 
   return (
     <main className="flex flex-col justify-center items-center h-screen gap-8">
-      <Menu/>
       <h1 className="text-5xl">Hello Algames !</h1>
     </main>
   );
